5eb928c5215de5eead21c7f9005ea18899149efd
luv-jeri <luvvjeri@gmail.com>
2022-12-02 21:07:19 +0530

    -

--- src/App.js
+++ src/App.js
@@ -1,51 +1,14 @@
-import { useState, useEffect } from 'react';
 import Header from './components/header/Header';
-import Card from './components/card/Card';
 import './App.css';
 import axios from 'axios';
+import { useState, useEffect } from 'react';
+import Card from './components/card/Card';
 
 const App = () => {
-  
-  const [user, setUser] = useState('');
-
-  let timeout = null;
-
-  const getUserDetails = (username) => {
-    if (timeout) clearTimeout(timeout);
-    timeout = setTimeout(async () => {
-      try {
-        const { data } = await axios.get(`https://api.github.com/users/${username}`);
-        setUser({
-          name: data.name,
-          avatar: data.avatar_url,
-          bio: data.bio,
-          company: data.company,
-          followers: data.followers,
-          following: data.following,
-          location: data.location,
-        });
-      } catch (error) {
-        console.log('User not found');
-        setUser(null);
-      }
-    }, 1000);
-  };
-
-  useEffect(() => {
-    getUserDetails(user);
-  }, [user]);
-
-  const handleInput = (e) => {
-    setUser(e.target.value);
-  };
-
   return (
     <div>
       <Header />
-      <section className='wrapper'>
-        <input onChange={handleInput} />
-        {user ? <Card user={user} /> : <h1>No user</h1>}
-      </section>
+      <Card />
     </div>
   );
 };
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,19 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Card.css';
-export default function Card({ user }) {
-  const { avatar, bio, company, followers, following, location, name } = user;
+
+export default function Card() {
+  const [value, setValue] = useState('title');
+  const [count, setCount] = useState(0);
+
+  const onMount = () => {
+    console.log('as soon as component mounts 🦄');
+    return () => {
+      console.log('just before component unmounts 😡');
+    };
+  };
+
+  useEffect(onMount, [value]);
+
   return (
     <div className='card-wrapper'>
-      <img src={avatar} alt={name} />
-      <h2>{name}</h2>
-      <div className='content'>
-        <div>
-          <h3>Bio</h3>
-          <span>{bio}</span>
+      <div className='card'>
+        <div className='card-image'>
+          <img
+            src={
+              'https://images.unsplash.com/photo-1669977041372-51bbee0b8a09?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+            }
+            alt='card'
+          />
         </div>
-        <div>
-          <h3>Company</h3>
-          <span>{company}</span>
+        <div className='card-content'>
+          <h2 className='card-title'>{value} </h2>
+          <p className='card-text'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.
+          </p>
+          <input
+            onChange={(e) => {
+              setValue(e.target.value);
+            }}
+            className='card-img-input'
+            placeholder='Enter Card Image URL'
+          />
+          <button
+            onClick={() => {
+              setCount(count + 1);
+            }}
+            className='card-btn'
+          >
+            Update Image
+          </button>
         </div>
       </div>
     </div>
